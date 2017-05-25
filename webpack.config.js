3f43e7591b3fdd9a04ee046c4adc21914fd48a66
Guolong <glliao@thoughtworks.com>
2017-05-25 09:53:54 +0800

    config the local environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
-module.exports = {
-    // entry: {
-    //     app:
-    // }
+var webpack = require('webpack');
 
+module.exports = {
+    entry: './server.js',
+    output: {
+        path: './server',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: 'style-loader!css-loader'}
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin('This file is created by webpack')
+    ]
 };
